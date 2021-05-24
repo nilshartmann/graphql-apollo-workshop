126e5a17c8e0b2ce587e5e05e67f8710f192f1c7
Nils Hartmann <nils@nilshartmann.net>
2021-05-24 19:37:23 +0200

    apollo config

--- app/apollo.config.js
+++ app/apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: {
-    includes: ["frontend/src/**/*.js"],
+    includes: ["./frontend_workspace/src/**/*.js"],
     service: {
       name: "project-app",
       url: "http://localhost:4000/graphql",
